66999da106589848b8d6f7d3c8ff994ada5c1424
BitPair <marchettimarcel9@gmail.com>
2025-07-07 02:35:29 +0200

    unix home routes

--- src/utils/path.rs
+++ src/utils/path.rs
@@ -8,16 +8,26 @@ pub fn get_route() -> String {
 
     let linux_regex = Regex::new(r"^(/home/)").unwrap();
     let windows_regex = Regex::new(r"^[A-Za-z]:[/\\]").unwrap();
+    let home_regex = Regex::new(r"^~[/\\]").unwrap();
 
-    if linux_regex.is_match(&args[1]) || windows_regex.is_match(&args[1]) {
-        return args[1].to_string();
+    let input_path = &args[1];
+    
+    if home_regex.is_match(input_path) {
+        if let Some(home_dir) = dirs::home_dir() {
+            let expanded_path = input_path.replacen("~", &home_dir.to_string_lossy(), 1);
+            return expanded_path;
+        }
     }
 
-    if args[1].starts_with("/") {
-        return  env::current_dir().unwrap().to_str().unwrap().to_string() + &args[1].to_string();
+    if linux_regex.is_match(input_path) || windows_regex.is_match(input_path) {
+        return input_path.to_string();
     }
 
-    return env::current_dir().unwrap().to_str().unwrap().to_string() + "/" + &args[1].to_string();
+    if input_path.starts_with("/") {
+        return  env::current_dir().unwrap().to_str().unwrap().to_string() + &input_path.to_string();
+    }
+
+    return env::current_dir().unwrap().to_str().unwrap().to_string() + "/" + &input_path.to_string();
 }
 
 fn valid_arguments(args: &Vec<String>) {
